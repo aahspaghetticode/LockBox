18ca9c74e7b078a1241d588fc79b71e1fef8bc8d
aahspaghetticode <hgamerrevived@gmail.com>
2025-06-27 12:35:57 -0500

    compile new version

--- installer.py
+++ installer.py
@@ -1,22 +1,16 @@
 import os
 import sys
 import subprocess
-def is_root():
-    return os.geteuid() == 0
 
-def elevate_with_pkexec():
-    try:
-        script_abs_path = os.path.abspath(sys.argv[0])
-        print(f"Using script absolute path: {script_abs_path}")
-        subprocess.check_call(["pkexec", sys.executable, script_abs_path] + sys.argv[1:])
-    except subprocess.CalledProcessError:
-        sys.exit(1)
-
-if is_root() == False:
+if True:
     print("Downloading...")
     os.makedirs(os.path.expanduser("~/.local/share/LockBox/"), exist_ok=True)
-    subprocess.run("curl -L https://raw.githubusercontent.com/aahspaghetticode/LockBox/refs/heads/main/main -o ~/.local/share/LockBox/main", shell=True)
-    subprocess.run("chmod +x ~/.local/share/LockBox/main", shell=True)
+    subprocess.run("rm -r ~/.local/share/LockBox/dist", shell=True)
+    subprocess.run("python3 -m ensurepip --upgrade", shell=True)
+    subprocess.run("python3 -m pip install pyinstaller --break-system-packages", shell=True)
+    subprocess.run("curl -L https://raw.githubusercontent.com/aahspaghetticode/LockBox/refs/heads/main/main.py -o ~/.local/share/LockBox/main.py", shell=True)
+    subprocess.run("pyinstaller --onefile --noconsole --distpath ~/.local/share/LockBox/dist/ ~/.local/share/LockBox/main.py", shell=True)
+    subprocess.run("chmod +x ~/.local/share/LockBox/dist/main", shell=True)
     os.makedirs(os.path.expanduser("~/.local/share/icons/"), exist_ok=True)
     subprocess.run("curl -L https://raw.githubusercontent.com/aahspaghetticode/LockBox/refs/heads/main/Icon.png -o ~/.local/share/icons/LockBox.png", shell=True)
     desktop_file_path = os.path.join(os.path.expanduser("~"), ".local", "share", "applications", "LockBox.desktop")
@@ -26,7 +20,7 @@ Version=1.0
 Type=Application
 Name=LockBox
 Comment=Secure password storage
-Exec={os.path.expanduser("~")}/.local/share/LockBox/main
+Exec={os.path.expanduser("~")}/.local/share/LockBox/dist/main
 Icon={os.path.expanduser("~")}/.local/share/icons/LockBox.png
 Terminal=false
 Categories=Utility;
@@ -34,7 +28,5 @@ Categories=Utility;
     subprocess.run("chmod +x ~/.local/share/applications/LockBox.desktop", shell=True)
     subprocess.run("gtk-update-icon-cache ~/.local/share/icons/", shell=True)
     subprocess.run("update-desktop-database ~/.local/share/applications/", shell=True)
-    elevate_with_pkexec()
-    sys.exit(0)
 print("installing...")
 subprocess.run("sudo apt update && sudo apt install -y python3-gi python3-gi-cairo gir1.2-gtk-3.0 python3 python3-cryptography", shell=True)
